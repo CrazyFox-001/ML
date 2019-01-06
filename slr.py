dcee06eff584acec363164bda11620aaf8a39a5e
CrazyFox-001 <46416788+CrazyFox-001@users.noreply.github.com>
2019-01-06 17:56:42 +0530

    Update slr.py

--- slr.py
+++ slr.py
@@ -1,26 +1,15 @@
-
-
-import matplotlib.pyplot as plt
-import pandas as pd
-
-
+import pandas as ps
+import numpy as np
 import matplotlib.pyplot as mb
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-dataset = pd.read_csv('googleplaystore.csv')
-X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 1].values
-
-from sklearn.preprocessing import LabelEncoder ,OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, -1] = labelencoder_X.fit_transform(X[:, -1])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
+data = ps.read_csv('busfare.csv')
+X = data.iloc[:, :-1].values
+Y = data.iloc[:, 1].values
 
 
 training_x, test_x, traning_y, test_y = train_test_split(X, Y, test_size=0.3)
-
 
 reg = LinearRegression()
 reg.fit(training_x, traning_y)
@@ -32,3 +21,6 @@ mb.title ("Oil Price vs Bus Fare")
 mb.xlabel = ("Oil Price")
 mb.ylabel = ("Bus Fare")
 mb.show()
+
+
+
